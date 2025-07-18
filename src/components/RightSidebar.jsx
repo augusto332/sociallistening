e57efa3c553ddcaac74ed58b7404bcc25c3ac6f8
augusto332 <87154125+augusto332@users.noreply.github.com>
2025-07-17 22:31:36 -0300

    Alinea sidebar y ajusta tabs de orden

--- src/components/RightSidebar.jsx
+++ src/components/RightSidebar.jsx
@@ -23,7 +23,7 @@ export default function RightSidebar({
   return (
     <aside
       className={cn(
-        "w-64 bg-secondary shadow-md p-6 space-y-6 flex flex-col items-end rounded-lg self-start sticky top-8 h-[calc(100vh-2rem)]",
+        "w-64 bg-secondary shadow-md p-6 space-y-6 flex flex-col items-start rounded-lg self-start sticky top-8 h-[calc(100vh-2rem)]",
         className
       )}
     >
